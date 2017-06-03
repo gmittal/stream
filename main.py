ff4d73d89e073afb1e2eca03fd1f97c1dec54347
gmittal <gautam@mittal.net>
2017-06-03 16:14:17 -0700

    Add preview search and environment variables.

---
+++ main.py
@@ -0,0 +1,29 @@
+import os, requests
+from os.path import join, dirname
+from dotenv import load_dotenv
+from flask import Flask
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+app = Flask(__name__)
+
+# Returns a 30 second preview URL given a song name
+def search(query):
+    try:
+        trackID = requests.get('https://api.spotify.com/v1/search?q='+query+'&type=track', headers={
+            'Authorization': 'Bearer '+os.environ.get('SPOTIFY_ACCESS'),
+        }).json()["tracks"]["items"][0]["id"]
+    except IndexError:
+        return None
+
+    return requests.get('https://api.spotify.com/v1/tracks/'+trackID, headers={
+        'Authorization': 'Bearer '+os.environ.get('SPOTIFY_ACCESS'),
+    }).json()["preview_url"]
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9000)
