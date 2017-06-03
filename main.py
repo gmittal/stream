e3f8978c7b3f5bce91110b0fd8671870208dfbfb
gmittal <gautam@mittal.net>
2017-06-03 16:23:04 -0700

    Download previews as files.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, requests
+import os, requests, shutil, uuid
 from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask
@@ -20,6 +20,17 @@ def search(query):
     return requests.get('https://api.spotify.com/v1/tracks/'+trackID, headers={
         'Authorization': 'Bearer '+os.environ.get('SPOTIFY_ACCESS'),
     }).json()["preview_url"]
+
+def download_song(url):
+    file_id = str(uuid.uuid4())
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(join(dirname(__file__), file_id +'.mp3'), 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+    return file_id
+
+download_song(search('hello'))
 
 @app.route("/")
 def hello():
